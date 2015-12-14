643c82d7247e0f89cadd8fc2db456f6f68d44ecd
khrysro <bymask@gmail.com>
2015-12-14 11:43:44 -0300

    modificacion ruta instalacion

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ if [ -z $userpriv ]
     exit 1
 fi
 
-install_dir="/usr/share/apps/mccm"
-mcm_shell="/usr/share/apps/mccm/bin/mccm"
+install_dir="/usr/lib/mccm"
+mcm_shell="/usr/lib/mccm/bin/mccm"
 
 echo "1/3 Copying files to ${install_dir}"
 mkdir -p ${install_dir} 2>/dev/null
@@ -25,8 +25,8 @@ ln -s ${mcm_shell}  /usr/bin/mccm
 ln -s ${mcm_shell}  /usr/bin/mccm-qt
 
 echo "3/3 Creating menu-entry for Monocute Connections Manager"
-cp /usr/share/apps/mccm/qt/mccm_icon.xpm /usr/share/pixmaps/mccm.xpm
-cp /usr/share/apps/mccm/qt/mccm.desktop /usr/share/applications/
+cp /usr/lib/mccm/qt/mccm_icon.xpm /usr/share/pixmaps/mccm.xpm
+cp /usr/lib/mccm/qt/mccm.desktop /usr/share/applications/
 
 echo "Done. Monocute Connections Manager is ready"
 echo "Type mccm on a console or run mcm-qt from the Menu"
